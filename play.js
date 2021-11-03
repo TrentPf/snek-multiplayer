228037d291ebc9050e6de844d91d7f1c674f15fc
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-03 13:12:22 -0400

    Add data receiving

--- play.js
+++ play.js
@@ -7,5 +7,27 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
 // Begin game
 game.start()
