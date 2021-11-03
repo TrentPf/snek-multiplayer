fd9700a261a1cb27ef503efc03604cc5e2a0cc1b
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-03 13:27:38 -0400

    Move connect function

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const connect = require("./client");
 process.stdout.write('\x07');
 
 const { Game } = require('./src/Game')
@@ -7,24 +7,7 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
