eb80835a3dfab3476854d2f7b6df13bb86e08162
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-03 14:06:45 -0400

    Add terminate on ctrl-c

--- play.js
+++ play.js
@@ -7,10 +7,25 @@ const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting ...");
 connect();
 
 // Begin game
 game.start()
+
+setupInput();
